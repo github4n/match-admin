93500df8c64f7468679fa0cef3519ecb3169e321
Marco <sz547073165@qq.com>
2017-10-26 17:50:17 +0800

    添加mybatis和mvc的配置类

---
+++ src/main/java/com/match/admin/core/ServiceException.java
@@ -0,0 +1,17 @@
+package com.match.admin.core;
+
+/**
+ * Created by Administrator on 2017/10/26 0026.
+ */
+public class ServiceException extends RuntimeException {
+    public ServiceException() {
+    }
+
+    public ServiceException(String message) {
+        super(message);
+    }
+
+    public ServiceException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
