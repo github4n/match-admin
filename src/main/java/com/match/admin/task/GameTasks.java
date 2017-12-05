354e2ec6e3a2b6be6d7e66589a87c42c843f81e4
Marco <sz547073165@qq.com>
2017-12-06 01:46:39 +0800

    修改定时任务的bug

--- src/main/java/com/match/admin/task/GameTasks.java
+++ src/main/java/com/match/admin/task/GameTasks.java
@@ -1,5 +1,6 @@
 package com.match.admin.task;
 
+import com.alibaba.fastjson.JSONObject;
 import com.match.admin.util.EmailMisc;
 import com.match.admin.util.SpiderMisc;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -13,21 +14,24 @@ import java.util.Map;
 @Component
 public class GameTasks {
     private String task1EmailStr = "";
-    @Scheduled(fixedDelay = 5 * 60 * 1000)
+
+    @Scheduled(fixedDelay = 1 * 60 * 1000)
     public void task1() throws Exception {
         System.out.println(new Date());
         ArrayList<HashMap<String, String>> matchIdList = SpiderMisc.get70minMatchId();
-        System.out.println(String.format("70min比赛：%s",matchIdList));
+        System.out.println(String.format("70min比赛：%s", matchIdList));
         for (Map<String, String> temp : matchIdList) {
             ArrayList<Integer> noGoalList = SpiderMisc.getNoGoalCount(temp.get("matchId"));
             System.out.println(noGoalList);
-            if (noGoalList.indexOf(0) < 3 && noGoalList.indexOf(1) < 3) {
-                continue;
-            }else{
+            if (noGoalList.get(0) >= 3 || noGoalList.get(1) >= 3) {
                 task1EmailStr = String.format("%s<p>%s</p><p>%s</p><p>%s</p><p></p>", task1EmailStr, temp.get("league"), temp.get("matchTeam"), temp.get("matchGoal"));
-                EmailMisc.sendEmail("70min match",task1EmailStr);
-                task1EmailStr = "";
+            } else {
+                continue;
             }
+        }
+        if (!"".equals(task1EmailStr)) {
+            EmailMisc.sendEmail("70min match", task1EmailStr);
+            task1EmailStr = "";
         }
     }
 }
